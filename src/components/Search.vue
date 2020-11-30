cc3481cdee8fe99abcfd9b7c847c9e741117dcc7
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2020-11-29 21:36:54 -0500

    finished search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,50 +1,72 @@
 <template>
-  <div class="search-container" @click="revealSearch">
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      class="feather feather-search"
-    >
-      <circle cx="11" cy="11" r="8"></circle>
-      <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-    </svg>
-    <h3>click to search</h3>
-    <div class="search-results" :class="{'search-active':reveal}">
-      <ul>
-        <li></li>
-      </ul>
+  <div class="search-wrapper">
+    <form @change="searchPosts" @submit.prevent="">
+      <input
+        v-model="searchQuery"
+        type="text"
+        placeholder="Example: The Room"
+      />
+      <!-- <input name="Submit" type="submit" value="submit"> -->
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="feather feather-search"
+      >
+        <circle cx="11" cy="11" r="8"></circle>
+        <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
+      </svg>
+    </form>
+    <div class="search-results" :class="{ 'search-active': isSearching }">
+      <div>
+        <router-link
+          @click.native="selectPost"
+          v-for="result in searchResult"
+          :key="result.id"
+          :to="{ name: 'Post', params: { id: result.id } }"
+          v-html="result.title.rendered"
+        ></router-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapState } from "vuex";
 export default {
-data(){
-    return{
-reveal:false
-    }
-},
-  computed: {
-    ...mapState({
-      posts: (state) => state.posts,
-    }),
+  data() {
+    return {
+      searchQuery: "",
+      isTyping: false,
+      searchResult: [],
+      isSearching: false,
+    };
   },
-  methods:{
-      revealSearch(){
-          if (this.reveal){
-              this.reveal = false
-          } else {
-              this.reveal = true;
-          }
+  watch: {
+    isTyping: function (value) {
+      if (!value) {
+        this.searchPosts(this.searchQuery);
+        this.isSearching = true;
       }
-  }
+    },
+  },
+  methods: {
+    searchPosts: function () {
+      this.isSearching = true;
+      fetch(`http://3.89.20.61/wp-json/wp/v2/posts?search=${this.searchQuery}`)
+        .then((resp) => resp.json())
+        .then((result) => {
+          this.searchResult = result;
+        });
+    },
+    selectPost() {
+      this.isSearching = false;
+    },
+  },
 };
 </script>
 
@@ -54,36 +76,56 @@ $color-blue: #0099cc;
 $color-lightred: #ffe7ff;
 $color-lightblue: #b1bbed;
 
-.search-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  cursor: pointer;
-  width: 300px;
+.search-wrapper {
+  position: rleative;
+  form {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 300px;
+input, select{
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display:inline-block;
+    border: 1px solid $color-blue;
+    border-radius: 15px;
+    box-sizing: border-box;
+}
 
-  h3 {
-    color: lightgray;
-    transition: color 0.3s ease-in-out;
-    width: 200px;
-  }
-  svg {
-    stroke: lightgray;
-    width: 50%;
-    transition: stroke 0.3s ease-in-out;
-  }
+    svg {
+      stroke: lightgray;
+      width: 50%;
+      transition: stroke 0.3s ease-in-out;
+      cursor:pointer;
+    }
 
-  &:hover > h3,
-  &:hover > svg {
-    stroke: $color-red;
-    color: $color-red;
+    &:hover > svg {
+      stroke: $color-red;
+    }
   }
+  .search-results {
+    max-height: 0;
+    overflow: hidden;
 
-  .search-results{
-      max-height:0;
-      overflow:hidden;
+    a {
+      display: block;
+    }
+
+    a:hover {
+      background-color: $color-lightred;
+    }
   }
-  .search-active{
-      max-height:100%;
+  .search-active {
+    height: 500px;
+    max-height: 100%;
+    width: 400px;
+    position: absolute;
+    z-index: 1000;
+    background-color: white;
+    top: 75%;
+    left: 50%;
+    overflow-y: scroll;
   }
 }
 </style>
