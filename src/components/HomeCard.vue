5c04356e6926f4fe5d1eccdb9fc919caeb933b83
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2020-12-02 08:50:51 -0500

    hover feature for the home cards

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,8 +1,13 @@
 <template>
-  <router-link :to="{ name: 'Post', params: { id: post.id } }">
+  <router-link
+    @mouseover.native="mousedOver = true"
+    @mouseleave.native="mousedOver = false"
+    :to="{ name: 'Post', params: { id: post.id } }"
+  >
     <div class="post-card">
       <div>
         <img
+          :class="{ 'animate-homecard-image': mousedOver }"
           v-if="
             post._embedded['wp:featuredmedia'][0].media_details.sizes
               .medium_large !== undefined
@@ -10,12 +15,17 @@
           :src="postImageML"
           alt=""
         />
-        <img v-else :src="postImage" alt="" />
+        <img
+          :class="{ 'animate-homecard-image': mousedOver }"
+          v-else
+          :src="postImage"
+          alt=""
+        />
       </div>
       <div>
         <h2 v-html="post.title.rendered"></h2>
         <p v-html="post.excerpt.rendered"></p>
-        <h3>read</h3>
+          <h3>read</h3>
       </div>
     </div>
   </router-link>
@@ -25,6 +35,11 @@
 export default {
   props: {
     post: Object,
+  },
+  data() {
+    return {
+      mousedOver: false,
+    };
   },
   computed: {
     postImageML() {
@@ -46,15 +61,14 @@ $color-lightred: #ffe7ff;
 $color-lightblue: #b1bbed;
 
 .home-card-container {
-  width:30%;
-  background-color:white;
+  width: 30%;
+  background-color: white;
   border: 5px solid $color-blue;
-      border-radius: 15px;
- 
+  border-radius: 15px;
+
   a {
     text-decoration: none;
     .post-card {
-      
       width: 100%;
       height: 25rem;
       margin: 3% 1%;
@@ -63,7 +77,9 @@ $color-lightblue: #b1bbed;
       overflow: hidden;
       transition: border-color 0.3s ease-in-out;
 
-      &:hover,&:active,&:focus {
+      &:hover,
+      &:active,
+      &:focus {
         border-color: $color-red;
       }
 
@@ -72,32 +88,120 @@ $color-lightblue: #b1bbed;
         height: 250px;
         object-fit: cover;
         object-position: 0 10%;
-        transform: scale(1.1,1.1);
+        transform: scale(1.1, 1.1);
       }
+
+      div img.animate-homecard-image {
+        animation: flicker, warp;
+        animation-duration: 3s;
+        animation-fill-mode: forwards;
+        animation-iteration-count: infinite;
+      }
+
+      svg.play-button {
+        stroke-width: 2px;
+        fill: $color-blue;
+      }
+
       div:nth-child(2) {
         padding: 3%;
-        background-color:white;
-        height:100%;
-        position:relative;
-        
+        background-color: white;
+        height: 100%;
+        position: relative;
+
         h2,
         h3,
         p {
-          margin:0 0 2%;
+          margin: 0 0 2%;
         }
         h2 {
           text-align: left;
-          color:$color-red;
+          color: $color-red;
         }
         h3 {
           text-align: left;
-          position:absolute;
-          top:80%;
-          left:3%;
-          color:$color-blue;
+          position: absolute;
+          top: 80%;
+          left: 3%;
+          color: $color-blue;
         }
       }
     }
+  }
+}
+
+@keyframes flicker {
+  0% {
+    opacity: 1;
+  }
+
+  33% {
+    opacity: 1;
+  }
+
+  34% {
+    opacity: 0;
+  }
+
+  35% {
+    opacity: 1;
+  }
+
+  75% {
+    opacity: 1;
+  }
+
+  76% {
+    opacity: 0;
+  }
+
+  77% {
+    opacity: 1;
+  }
+
+  80% {
+    opacity: 1;
+  }
+
+  81% {
+    opacity: 0;
+  }
+
+  82% {
+    opacity: 1;
+  }
+}
+
+@keyframes warp {
+  0% {
+    transform: scale(1.1, 1.1);
+  }
+  10% {
+    transform: scale(1.1, 1.1);
+  }
+  11% {
+    transform: scale(3.5, 1.1);
+  }
+  17% {
+    transform: scale(3.5, 1.1);
+  }
+  18% {
+    transform: scale(1.1, 1.1);
+  }
+
+  55% {
+    transform: scale(1.1, 1.1);
+  }
+
+  56% {
+    transform: scale(3.5, 2);
+  }
+
+  58% {
+    transform: scale(1.1, 1.1);
+  }
+  100% {
+    transform: scale(1.1, 1.1);
   }
 }
 </style>
