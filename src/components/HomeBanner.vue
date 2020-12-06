3edca60d0318071588280107f0088e61d2869c52
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2020-12-06 14:31:20 -0500

    stylize home banner

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="home-banner h-96 w-screen bg-fixed bg-top bg-no-repeat bg-cover">
-    <div class="home-banner-content-container w-9/12 m-auto relative">
-      <h1 class="text-white">Our Ultimate List of Bad Movies</h1>
-      <h3 class="text-white">
+    <div class="home-banner-content-container py-3 lg:w-9/12 w-11/12 m-auto">
+      <h1 class="text-white mb-12">Our Ultimate List of Bad Movies</h1>
+      <h3 class="text-white mb-12">
         Looking to dip your toes into the waters of bad movies?<br />Check out
         our ultimate list of movies sure to make you chuckle.
       </h3>
       <router-link
-        class="banner-button banner-button-position"
+        class="banner-button"
         :to="{ name: 'Post', params: { id: 23 } }"
       >
-      <h2 class="font-mono text-white font-bold">Start here</h2>
+      <h2>Start here</h2>
       </router-link>
     </div>
   </div>
@@ -31,22 +31,5 @@ export default {
 .home-banner {
   background: url(https://www.api-sourscreen.com/wp-content/uploads/2020/11/the-room.jpg);
   translate: scale(1.1, 1.1);
-
-  .home-banner-content-container {
-    h1 {
-      position: absolute;
-      top: 5rem;
-    }
-
-    h3 {
-      position: absolute;
-      top: 10rem;
-    }
-
-    .banner-button-position {
-      position: absolute;
-      top: 15rem;
-    }
-  }
 }
 </style>
