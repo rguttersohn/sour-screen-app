424326e60db2d7e3e3de68cd4ca7b1d4fe603b08
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2020-12-08 21:37:39 -0500

    grid implemented

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -1,44 +1,63 @@
 <template>
-  <div class="post-container" >
-    <router-view name="PostContent">
-      <h2>test</h2>
-    </router-view>
-    <div v-if="currentPost !== ''">
-      <img :src="currentPost.jetpack_featured_media_url" />
-      <!-- <h1 v-html="currentPost.title.rendered"></h1> -->
-      <div
-        v-html="currentPost.excerpt.rendered"
-        class="post-excerpt-container"
-      ></div>
-      <div
-        v-html="currentPost.content.rendered"
-        class="post-content-container"
-      ></div>
-    </div>
-    <section class="related-content">
-       <h2>Related Content:</h2>
-       <p v-if="currentPost.title !== undefined">If you enjoyed {{currentPost.title.rendered}}, we think you'll like:</p>
-      <div class="related-content-container" v-for="relatedPost in relatedPosts" :key="relatedPost.id" :data-id="relatedPost.id">
-          <RelatedCard :post='relatedPost' />
+  <div class="w-screen lg:py-36 py-24">
+    <section class="w-full" id="intro-section" v-if="currentPost !== ''">
+      <div class="w-9/12 m-auto">
+        <h1
+          v-html="currentPost.title.rendered"
+          class="text-red-light mb-6"
+        ></h1>
+        <div class="mb-6 excerpt" v-html="currentPost.excerpt.rendered"></div>
       </div>
     </section>
+
+    <div class="grid grid-cols-6 grid-rows-auto">
+      <section
+        id="post-section"
+        class="m-auto w-full md:w-3/4 col-span-6 md:col-span-4 md:row-span-1"
+        v-if="currentPost !== ''"
+      >
+        <div
+          v-html="currentPost.content.rendered"
+          class="post-content-container"
+        ></div>
+      </section>
+      <section
+        class="m-auto w-full col-span-6 md:w-3/4 md:col-span-2 md:row-span-2"
+      >
+        <h2>Related Content:</h2>
+        <p v-if="currentPost.title !== undefined">
+          If you enjoyed {{ currentPost.title.rendered }}, we think you'll like:
+        </p>
+        <div
+          class="related-content-container"
+          v-for="relatedPost in relatedPosts"
+          :key="relatedPost.id"
+          :data-id="relatedPost.id"
+        >
+          <RelatedCard :post="relatedPost" />
+        </div>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapState } from "vuex";
-import RelatedCard from '@/components/RelatedCard.vue';
+import RelatedCard from "@/components/RelatedCard.vue";
 export default {
-  name:"Post",
-  components:{RelatedCard},
+  name: "Post",
+  components: { RelatedCard },
   data() {
     return {
       currentPost: "",
       id: this.$route.params.id,
-      relatedPosts: []
+      relatedPosts: [],
     };
   },
   computed: {
+    postImage() {
+      return this.currentPost.jetpack_featured_media_url;
+    },
     ...mapState({
       baseAPIURL: (state) => state.baseAPIURL,
       posts: (state) => state.posts,
@@ -52,62 +71,53 @@ export default {
       });
   },
   methods: {
-    pushToRelated() {      
+    pushToRelated() {
       this.posts.forEach((post) => {
         post.relatedScore = 0;
         if (post.id !== this.currentPost.id) {
-          post._embedded['wp:term'][0].forEach(el=>{
-            for(let i  = 0;i < this.currentPost._embedded['wp:term'][0].length;i++){
-              if (el.name === this.currentPost._embedded['wp:term'][0][i].name){
-                post.relatedScore = post.relatedScore + 3
+          post._embedded["wp:term"][0].forEach((el) => {
+            for (
+              let i = 0;
+              i < this.currentPost._embedded["wp:term"][0].length;
+              i++
+            ) {
+              if (
+                el.name === this.currentPost._embedded["wp:term"][0][i].name
+              ) {
+                post.relatedScore = post.relatedScore + 3;
               }
             }
-          })
-          post._embedded['wp:term'][1].forEach(el=>{
-            for(let i  = 0;i < this.currentPost._embedded['wp:term'][1].length;i++){
-              if (el.name === this.currentPost._embedded['wp:term'][1][i].name){
-                post.relatedScore = post.relatedScore + 1
+          });
+          post._embedded["wp:term"][1].forEach((el) => {
+            for (
+              let i = 0;
+              i < this.currentPost._embedded["wp:term"][1].length;
+              i++
+            ) {
+              if (
+                el.name === this.currentPost._embedded["wp:term"][1][i].name
+              ) {
+                post.relatedScore = post.relatedScore + 1;
               }
             }
-          })
+          });
         }
       });
-      this.relatedPosts = this.posts.sort((a,b)=>a.relatedScore - b.relatedScore).reverse().slice(0,5)
-    }
+      this.relatedPosts = this.posts
+        .sort((a, b) => a.relatedScore - b.relatedScore)
+        .reverse()
+        .slice(0, 5);
+    },
   },
   watch: {
-   currentPost: function () {
-     if (this.posts.length > 0){
-      this.pushToRelated();
-     }
+    currentPost: function () {
+      if (this.posts.length > 0) {
+        this.pushToRelated();
+      }
     },
-  }
+  },
 };
 </script>
 
 <style lang="scss">
-.post-container {
-  width: 100vw;
-
-  img {
-    width: 100vw;
-    height: 400px;
-    object-fit: cover;
-    object-position: 0 5%;
-  }
-
-  .post-excerpt-container p {
-    font-family: Georgia, "Times New Roman", Times, serif;
-    font-size: 18px;
-    font-style: normal;
-    font-variant: normal;
-    font-weight: 700;
-    line-height: 15.4px;
-  }
-
-  .post-content-container {
-    width: 75%;
-    margin: auto;
-  }
-}
 </style>
