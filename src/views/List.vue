dd5ed1806227fd39ecc5c17aec6a9484c00ed3e5
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2020-12-19 19:28:13 -0500

    styled lists page

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,22 +1,22 @@
 <template>
-  <div class="w-screen py-24 lg:py-36 h-screen"  id="lists-list">
+  <div class="w-screen py-24 lg:py-36 h-screen">
     <div class="w-3/4 m-auto">
       <h1 class="text-center text-blue-main">Our lists:</h1>
       <div 
-      class="border-4 border-red-main rounded-lg w-full md:w-3/4 m-auto  h-64 my-3 overflow-hidden"
+      class=""
       v-for="list in lists" :key="list.id">
-        <PostCard :post="list" />
+        <ListCard :post="list" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import PostCard from "@/components/PostCard.vue";
+import ListCard from "@/components/ListCard.vue";
 
 export default {
   name:"Lists",
-  components: { PostCard },
+  components: { ListCard },
   computed: {
     lists() {
       return this.$store.getters.lists;
