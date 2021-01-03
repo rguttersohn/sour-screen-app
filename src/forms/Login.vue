cfa2b39b757e415d74e021958afa55a6fffe906a
Robert Guttersohn <robertguttersohn@Roberts-MacBook-Pro.local>
2021-01-03 18:11:56 -0500

    small fixes to mobile version

--- src/forms/Login.vue
+++ src/forms/Login.vue
@@ -1,24 +1,16 @@
 <template>
-  <div
-  id="forms-page-sign-up"
-  >
-  <form
-  @submit.prevent="handleLogin"
-  >
-  <label for="username">User Name</label>
-  <input
-  type="text"
-  placeholder="User Name"
-  v-model="loginInfo.username"
-  />
-  <label for="username">User Name</label>
-  <input
-  type="password"
-  placeholder="Password"
-  v-model="loginInfo.password"
-  />
-  <input class="cursor-pointer" type="submit" value="Login" />
-  </form>
+  <div id="forms-page-sign-up">
+    <form @submit.prevent="handleLogin">
+      <label for="username">User Name</label>
+      <input type="text" placeholder="User Name" v-model="loginInfo.username" />
+      <label for="username">User Name</label>
+      <input
+        type="password"
+        placeholder="Password"
+        v-model="loginInfo.password"
+      />
+      <input class="cursor-pointer" type="submit" value="Login" />
+    </form>
   </div>
 </template>
 
@@ -30,30 +22,34 @@ export default {
         username: "",
         password: "",
       },
-    }
+    };
   },
-  methods:{
+  methods: {
     handleLogin() {
-      this.$store.dispatch("submitLogin",this.loginInfo);
+      this.$store.dispatch("submitLogin", this.loginInfo);
     },
-  }
+  },
 };
 </script>
 
 <style>
-footer #forms-page-sign-up{
-    @apply flex flex-col lg:flex-row justify-evenly lg:justify-start items-stretch sm:h-40 md:h-10 flex-wrap
+footer #forms-page-sign-up {
+  @apply sm:h-40 md:h-10 flex-wrap;
 }
-#forms #forms-page-sign-up{
+
+footer #forms-page-sign-up form{
+  @apply flex flex-col lg:flex-row justify-evenly lg:justify-start items-stretch;
+}
+#forms #forms-page-sign-up {
   @apply w-full md:w-11/12 lg:w-3/4 m-auto p-5 bg-blue-xLight;
 }
 
-footer #forms-page-sign-up form input, #forms #forms-page-sign-up form input{
+footer #forms-page-sign-up form input,
+#forms #forms-page-sign-up form input {
   @apply mb-5 p-3;
 }
 
-#forms #forms-page-sign-up form{
+#forms #forms-page-sign-up form {
   @apply flex flex-col w-full md:w-96 m-auto;
 }
-
 </style>
--- src/views/Forms.vue
+++ src/views/Forms.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="forms" class="w-screen lg:py-36 py-24 h-screen">
-    <div class="w-2/4 mx-auto my-10">
+    <div class="w-11/2 md:w-2/4 mx-auto my-10">
       <div class="flex justify-evenly mx-auto w-full mb-0">
     <h2 class="text-white bg-red-xLight py-3 w-1/4 text-center cursor-pointer" @click="toSignUp">Sign Up</h2>
     <h2 class="text-white bg-blue-xLight py-3 w-1/4 text-center cursor-pointer" @click="toLogin">Login</h2>
